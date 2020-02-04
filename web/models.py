6dbe100a8e13b8e4a5e2b3d8a6f67fc8125d078c
mrz <marzieh.hashemipour@gmail.com>
2020-02-04 17:21:09 +0100

    second commit

--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
+from __future__ import unicode_literals
+
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class Expense(models.Model):
+    now = datetime.now()
+    Date = models.DateTimeField()
+    txt = models.CharField(max_length=255)
+    amount = models.BigIntegerField()
+    usr = models.ForeignKey(User,on_delete=models.PROTECT) 
+
