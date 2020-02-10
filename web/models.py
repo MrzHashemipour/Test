ed51acf61b20d84b7ac66d135f513bf83ad14851
mrz <marzieh.hashemipour@gmail.com>
2020-02-10 12:26:04 +0100

    income is added

--- web/models.py
+++ web/models.py
@@ -2,13 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
 
 # Create your models here.
 class Expense(models.Model):
-    now = datetime.now()
     Date = models.DateTimeField()
     txt = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     usr = models.ForeignKey(User,on_delete=models.PROTECT) 
 
+    def __str__(self):
+        return "{}---{}---{}".format(self.txt, self.Date, self.amount)
+        #return self.txt
+
+class Income(models.Model):
+    Date = models.DateTimeField()
+    txt = models.CharField(max_length = 255)
+    amount = models.BigIntegerField()
+    usr = models.ForeignKey(User,on_delete = models.PROTECT)
+    def __str__(self):
+        return "{}---{}---{}".format(self.txt, self.Date, self.amount)
